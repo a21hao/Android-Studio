171d90cca6a29921a9343f82e82628d0962c63df
a21hao <94469307+a21hao@users.noreply.github.com>
2023-11-08 19:03:28 +0100

    first commit

---
+++ prueba/app/src/main/java/com/example/prueba/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.prueba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.prueba.adapter.SuperHeroAdapter
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView(){
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerSuperHero)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = SuperHeroAdapter(SuperHeroProvider.superheroList)
+    }
+}
---
+++ prueba/app/src/main/java/com/example/prueba/SuperHeroProvider.kt
@@ -0,0 +1,38 @@
+package com.example.prueba
+
+class SuperHeroProvider {
+    companion object{
+        val superheroList = listOf<SuperHero>(
+            SuperHero(
+                "Jumanji",
+                "James Cameron",
+                "Avatar",
+                R.drawable.jumanji
+            ),
+            SuperHero(
+                "Avatar",
+                "James Cameron",
+                "Avatar",
+                R.drawable.avatar_3gqz
+            ),
+            SuperHero(
+                "Avatar",
+                "James Cameron",
+                "Avatar",
+                R.drawable.avatar_3gqz
+            ),
+            SuperHero(
+                "Avatar",
+                "James Cameron",
+                "Avatar",
+                R.drawable.avatar_3gqz
+            ),
+            SuperHero(
+                "Avatar",
+                "James Cameron",
+                "Avatar",
+                R.drawable.avatar_3gqz
+            ),
+        )
+    }
+}
