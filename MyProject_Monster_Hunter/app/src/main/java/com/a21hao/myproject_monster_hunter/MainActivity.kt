5b28ef960320145bc3833179fa9d8a05126c4c17
a21hao <94469307+a21hao@users.noreply.github.com>
2024-01-24 19:07:01 +0100

    project_monster_hunter

---
+++ MyProject_Monster_Hunter/app/src/main/java/com/a21hao/myproject_monster_hunter/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.a21hao.myproject_monster_hunter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
