ead33b076ef608bad164a5f104f7a818819be925
a21hao <94469307+a21hao@users.noreply.github.com>
2024-02-14 16:53:49 +0100

    no message

--- app/src/main/java/com/a21hao/reproductordemsica/MainActivity.kt
+++ app/src/main/java/com/a21hao/reproductordemsica/MainActivity.kt
@@ -2,6 +2,7 @@ package com.a21hao.reproductordemsica
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.layoutvertical.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
