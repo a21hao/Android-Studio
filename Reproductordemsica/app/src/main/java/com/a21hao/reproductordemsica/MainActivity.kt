be0c6c21db373cd411367bfa9cd46aad980c2e53
a21hao <94469307+a21hao@users.noreply.github.com>
2024-02-20 20:55:16 +0100

    no message

--- Reproductordemsica/app/src/main/java/com/a21hao/reproductordemsica/MainActivity.kt
+++ Reproductordemsica/app/src/main/java/com/a21hao/reproductordemsica/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.a21hao.reproductordemsica
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.tu_archivo_de_musica)
+
+        // Reproducir música cuando la actividad se inicia
+        mediaPlayer.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
     }
 }
