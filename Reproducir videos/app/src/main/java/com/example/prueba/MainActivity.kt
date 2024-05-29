3da3303e439f335c8774fed776409baa87b6149d
Haojie Lin <94469307+a21hao@users.noreply.github.com>
2024-05-29 16:54:36 +0200

    Revert "2"
    This reverts commit 2c1d6a13b9a6e5a3137f5275d3358c6c26526806.

--- Reproducir videos/app/src/main/java/com/example/prueba/MainActivity.kt
+++ Reproducir videos/app/src/main/java/com/example/prueba/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.prueba
 
-import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.widget.MediaController
@@ -8,32 +7,34 @@ import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private var mediaPlayer: MediaPlayer? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Referencias a los VideoView en el diseño
+        val videoViewOnline: VideoView = findViewById(R.id.videoViewOnline)
         val videoViewLocal: VideoView = findViewById(R.id.videoViewLocal)
 
+        // Configurar la URL del video en línea
+        val videoUrlOnline = "https://www.dailymotion.com/video/x8w6p8c"
+        val videoUriOnline = Uri.parse(videoUrlOnline)
+        videoViewOnline.setVideoURI(videoUriOnline)
+
+        // Configurar el video local, que debe estar en la carpeta "raw" del proyecto
         val videoPathLocal =
-            "android.resource://" + packageName + "/" + R.raw.mariovideo
+            "android.resource://" + packageName + "/" + R.raw.video
         val videoUriLocal = Uri.parse(videoPathLocal)
         videoViewLocal.setVideoURI(videoUriLocal)
+
+        // Añadir controles de reproducción
+        val mediaControllerOnline = MediaController(this)
+        videoViewOnline.setMediaController(mediaControllerOnline)
 
         val mediaControllerLocal = MediaController(this)
         videoViewLocal.setMediaController(mediaControllerLocal)
 
-        videoViewLocal.setOnPreparedListener {
-            videoViewLocal.start()
-        }
-
-        mediaPlayer?.start()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mediaPlayer?.release()
+        // Comenzar la reproducción de los videos
+        videoViewOnline.start()
+        videoViewLocal.start()
     }
 }
